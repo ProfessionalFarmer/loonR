20b1b67776150c4850a2a692a77207023f68709f
Zhongxu <sxzhuxu@126.com>
2020-06-19 17:09:12 +0800

    add a new R file

---
+++ R/TCGA.R
@@ -0,0 +1,33 @@
+
+#' Get sample barcode and TCGA project map information
+#' 
+#'
+#' @param project A vector. 
+#'  TCGA-ACC, TCGA-COAD. 
+#'  See getGDCprojects()$project_id
+#' @return A data.frame
+#'  X1 proj samples
+#'	1	1	TCGA-ACC	TCGA-OR-A5JZ
+#'	2	1	TCGA-ACC	TCGA-OR-A5JK
+#' @export
+#'
+#' @examples
+GetSampleProjectInfoMap <- function(project=NA){
+  
+  library(TCGAbiolinks)
+  
+  # If not specify project, load default
+  if (project==NA){
+    project <- grep("TCGA", sort(getGDCprojects()$project_id), 
+                    value = TRUE)
+  }
+  
+  
+  df <- plyr::adply(project, .margins = 1, .fun = function(proj) {
+    samples <- TCGAbiolinks:::getSubmitterID(proj)
+    return(data.frame(proj, samples))
+  })
+  
+  return(df)
+  
+}

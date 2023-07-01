5a1267034bd60db70802dc144452022e318d1d9a
Zhongxu <sxzhuxu@126.com>
2023-07-01 23:48:59 +0800

    update a function

--- R/GSE.R
+++ R/GSE.R
@@ -668,45 +668,19 @@ compare_GSE.HTSAnalyzer <- function(rna.df.log, group, prefix="Group", customGS=
   gse.res.single.table$Adjusted.Pvalue <- gse.res.single.table$Adjusted.Pvalue * sign(gse.res.single.table$Observed.score)
 
   # subset data frame
-  gse.res.single.table <- gse.res.single.table[,c("Gene.Set.Term","Adjusted.Pvalue","GroupName")]
+  gse.res.single.table <- gse.res.single.table[,c("Gene.Set.Term", "GSType", "Adjusted.Pvalue", "GroupName")]
 
   # cast data frame
-  gse.res.single.table <- reshape::cast(gse.res.single.table, Gene.Set.Term + GSType ~GroupName, value = "Adjusted.Pvalue", fun.aggregate = mean)
+  gse.res.single.table <- reshape::cast(gse.res.single.table, Gene.Set.Term + GSType ~ GroupName, value = "Adjusted.Pvalue", fun.aggregate = mean)
   row.names(gse.res.single.table) <- gse.res.single.table$Gene.Set.Term
+
+
   result$heatmap.df.row.type = gse.res.single.table$GSType
   names(result$heatmap.df.row.type) = gse.res.single.table$Gene.Set.Term
+  result$heatmap.df.includeGSType =gse.res.single.table
 
   gse.res.single.table <- as.data.frame(gse.res.single.table[,-c(1,2)])
   result$heatmap.df = gse.res.single.table
-
-  heatmap.by.type = function(df, type = NULL){
-
-    if(!is.null(type)){
-      tmp.df = df %>% filter(GSType == type) # MSig_H PW_KEGG GO_BP MSig_H
-    }
-
-    tmp.df$Adjusted.Pvalue <- -log10(tmp.df$Adjusted.Pvalue)
-    tmp.df[which(is.infinite(tmp.df$Adjusted.Pvalue)), ]$Adjusted.Pvalue <- cutoff.log10
-    tmp.df$Adjusted.Pvalue[tmp.df$Adjusted.Pvalue >=
-                                       cutoff.log10] <- cutoff.log10
-    tmp.df$Adjusted.Pvalue <- tmp.df$Adjusted.Pvalue *
-      sign(tmp.df$Observed.score)
-    tmp.df <- tmp.df[, c(
-      "Gene.Set.Term",
-      "Adjusted.Pvalue", "GroupName"
-    )]
-    tmp.df <- reshape::cast(tmp.df,
-                                      Gene.Set.Term ~ GroupName,
-                                      value = "Adjusted.Pvalue",
-                                      fun.aggregate = mean
-    )
-    row.names(tmp.df) <- tmp.df$Gene.Set.Term
-    tmp.df <- as.data.frame(tmp.df[
-      ,
-      -c(1)
-    ])
-
-  }
 
 
   result

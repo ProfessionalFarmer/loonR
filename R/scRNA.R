0a9d9272504410a457c753c92182996282f425df
Jason <sxzhuxu@126.com>
2024-08-01 09:39:11 +0800

    update scatter plot with bar for scRNA

--- R/scRNA.R
+++ R/scRNA.R
@@ -808,3 +808,132 @@ runCytoTrace2 <- function(seurat.obj, cores = 20){
 }
 
 
+
+
+#' Plot dim plot and cell count bar
+#'
+#' @param object
+#' @param color
+#' @param dim
+#' @param rm.axis
+#' @param cell.id
+#' @param bar.width
+#' @param point.size
+#' @param rm.barplot
+#' @param legend.psize
+#' @param arrow.len
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # modified from scRNAtoolVis::scatterCellPlot
+#'
+dimPlotWithCountBar <- function (object = NULL, color = NULL, dim = "umap", rm.axis = FALSE,
+          cell.id = NULL, bar.width = 0.2, point.size = 1, rm.barplot = FALSE,
+          legend.psize = 1.5, arrow.len = 0.2)
+{
+  reduc <- data.frame(Seurat::Embeddings(object, reduction = dim))
+  meta <- object@meta.data
+  pc12 <- cbind(reduc, meta)
+  pc12$idents <- as.character(Seurat::Idents(object))
+  if (is.null(cell.id)) {
+    cell_num <- dplyr::arrange(dplyr::summarise(dplyr::group_by(pc12,
+                                                                idents), n = dplyr::n()), n)
+  }
+  else {
+    cell_num <- dplyr::arrange(dplyr::summarise(dplyr::group_by(pc12,
+                                                                idents, .data[[cell.id]]), n = dplyr::n()), n)
+  }
+  if (rm.axis == FALSE) {
+    lab.shift = unit(-2.5, "lines")
+  }
+  else {
+    lab.shift = unit(-1, "lines")
+  }
+  grid.newpage()
+  pushViewport(viewport(x = unit(0.1, "npc"), y = unit(0.5,
+                                                       "npc"), width = unit(0.5, "npc"), height = unit(0.7,
+                                                                                                       "npc"), just = "left", xscale = grDevices::extendrange(range(pc12[,
+                                                                                                                                                                         1]), f = 0.05), yscale = grDevices::extendrange(range(pc12[,
+                                                                                                                                                                                                                                    2]), f = 0.05), ))
+  grid.rect()
+  if (rm.axis == FALSE) {
+    jjPlot::grid.xaxis2(label.space = 0.5)
+    jjPlot::grid.yaxis2(label.space = 0.25)
+  }
+  celltype <- cell_num$idents
+  if (is.null(color)) {
+    cols <- circlize::rand_color(n = length(celltype))
+  }
+  else {
+    cols <- color
+  }
+  ################# modified by myself 2024-08-01
+  if(!is.null(names(cols))){
+  cols = cols[celltype]
+  }
+  # # ## ##### ##### #### ## ###
+
+  for (i in seq_along(celltype)) {
+    tmp <- pc12[which(pc12$idents %in% celltype[i]), ]
+    grid.points(x = tmp[, 1], y = tmp[, 2], pch = 19, size = unit(point.size,
+                                                                  "pt"), gp = gpar(col = cols[i]))
+  }
+  if (rm.axis == TRUE) {
+    grid.segments(x0 = 0.025, x1 = arrow.len, y0 = 0.05,
+                  y1 = 0.05, arrow = arrow(length = unit(2, "mm"),
+                                           type = "closed"), gp = gpar(fill = "black"))
+    grid.text(label = paste0(toupper(dim), " 1"), x = (arrow.len +
+                                                         0.025)/2, y = 0.025, gp = gpar(fontsize = 6, fontface = "bold.italic"))
+    grid.segments(x0 = 0.05, x1 = 0.05, y0 = 0.025, y1 = arrow.len,
+                  arrow = arrow(length = unit(2, "mm"), type = "closed"),
+                  gp = gpar(fill = "black"))
+    grid.text(label = paste0(toupper(dim), " 2"), x = 0.025,
+              y = (arrow.len + 0.025)/2, rot = 90, gp = gpar(fontsize = 6,
+                                                             fontface = "bold.italic"))
+  }
+  else {
+    grid.text(label = paste0(toupper(dim), " dimension 1"),
+              x = 0.5, y = lab.shift)
+    grid.text(label = paste0(toupper(dim), " dimension 2"),
+              x = lab.shift, y = 0.5, rot = 90)
+  }
+  popViewport()
+  if (isFALSE(rm.barplot)) {
+    pushViewport(viewport(x = unit(0.61, "npc"), y = unit(0.5,
+                                                          "npc"), width = unit(bar.width, "npc"), height = unit(0.7,
+                                                                                                                "npc"), just = "left", yscale = c(0, nrow(cell_num) +
+                                                                                                                                                    0.75), xscale = c(0, max(cell_num$n) + 0.1 * max(cell_num$n))))
+    if (rm.axis == FALSE) {
+      jjPlot::grid.xaxis2(label.space = 0.5, at = c(0,
+                                                    max(cell_num$n)), labels = as.character(c(0,
+                                                                                              max(cell_num$n))))
+    }
+    grid.rect(x = rep(0, nrow(cell_num)), y = 1:nrow(cell_num),
+              width = cell_num$n, height = unit(0.08, "npc"),
+              just = "left", gp = gpar(fill = cols, col = NA),
+              default.units = "native")
+    grid.rect(gp = gpar(fill = "transparent"))
+    grid.text(label = "Number of cells", x = 0.5, y = lab.shift)
+    popViewport()
+  }
+  if (isTRUE(rm.barplot)) {
+    bar.width = 0
+  }
+  pushViewport(viewport(x = unit(0.61 + bar.width, "npc"),
+                        y = unit(0.5, "npc"), width = unit(0.2, "npc"), height = unit(0.7,
+                                                                                      "npc"), just = "left", yscale = c(0, nrow(cell_num) +
+                                                                                                                          0.75)))
+  grid.points(x = rep(0.1, nrow(cell_num)), y = 1:nrow(cell_num),
+              pch = 19, gp = gpar(col = cols), size = unit(legend.psize,
+                                                           "char"))
+  if (!is.null(cell.id)) {
+    grid.text(label = as.character(unlist(cell_num[, cell.id])),
+              x = 0.1, y = 1:nrow(cell_num), default.units = "native")
+  }
+  grid.text(label = cell_num$idents, x = 0.2, y = 1:nrow(cell_num),
+            just = "left", gp = gpar(fontsize = 10), default.units = "native")
+  popViewport()
+}
+
